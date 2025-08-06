6958284f3272241cb7b4638d4b8a4bbdcce67ad8
mohammadjavadahmadi68-droid <mohammadjavadahmadi68@gmail.com>
2025-08-06 23:43:25 +0330

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    const canvas = document.getElementById('progressChart');
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(10, 50, 50, 70);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(70, 30, 50, 90);
-});
